e8c3601879815fb3fda2830920a0487271c54889
Edi Aquino de Souza <edsouzza@hotmail.com>
2023-11-01 21:10:56 -0300

    finalized banner

---
+++ alurabooks/src/components/banner/Banner.tsx
@@ -0,0 +1,16 @@
+import Input from '../input/Input'
+import './Banner.css'
+
+export default function Banner(){
+
+   return(
+      <section className="banner">
+        <div className="banner__textos">
+           <h1 className="banner__h1">já sabe por onde começar?</h1>
+           <p className="banner__descricao">encontre em nossa estante o que precisa para seu desenvolvimento!</p>
+        </div>
+        <Input/>
+      </section>
+   )
+
+}
---
+++ alurabooks/src/components/input/Input.tsx
@@ -0,0 +1,11 @@
+import './Input.css'
+
+export default function Input(){
+
+    return(
+        <div className="banner__areainput">
+            <input type="text" className="banner__pesquisa" placeholder="Qual será sua próxima leitura?"/>            
+        </div>
+    )
+
+}
